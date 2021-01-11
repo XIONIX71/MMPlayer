907277b846899501ceb2cd5657ca92aa5cb9d31a
Lucas Massot <lm657000@gr10-21.esirem-ad.ad.u-bourgogne.fr>
2021-01-11 14:41:05 +0100

    sauvegarde du premier merge de l' autre repo

---
+++ MMPlayer/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MMPlayer
+//
+//  Created by Sam Maroun on 08/01/2021.
+//
+
+import UIKit
+import AVFoundation
+import MediaPlayer
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
