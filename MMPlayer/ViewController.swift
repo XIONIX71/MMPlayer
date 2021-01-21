a2c509075ba9b9db6703b533b2e2b6fbe67d8f3a
Lucas MASSOT <zeus@DESKTOP-AD5J5F6.localdomain>
2021-01-21 12:01:02 +0100

    ajout des boutons de sam

--- MMPlayer/ViewController.swift
+++ MMPlayer/ViewController.swift
@@ -44,6 +44,72 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
     
     var isRunning = false
 
+      @IBAction func playSong(_ sender: Any) {
+        
+        let play = UIImage(named: "play")
+        let pause = UIImage(named:"pause")
+        
+        if shuffleState == true{
+            shuffleArray.removeAll()
+        }
+        
+        else if audioPlayer.isPlaying{
+            pauseAudioPlayer()
+        }
+        else{
+            playAudio()
+        }
+        
+        playButton.setImage(audioPlayer.isPlaying ? pause: play, for:UIControl.State())
+    }
+    
+    @IBAction func nextSong(_ sender: Any) {
+        playNextAudio()
+    }
+    
+    
+    @IBAction func previousSong(_ sender: Any) {
+        playPreviousAudio()
+    }
+    
+    
+    @IBAction func changeSliderLocation(_ sender: UISlider) {
+        
+        audioPlayer.currentTime = TimeInterval(sender.value)
+    }
+    
+    @IBAction func shuffleButtonTapped(_ sender: UIButton) {
+        shuffleArray.removeAll()
+        
+        if sender.isSelected == true{
+            sender.isSelected = false
+            shuffleState = false
+            
+            UserDefaults.standard.set(false, forKey: "shuffleState")
+        }
+        else{
+            sender.isSelected = true
+            shuffleState = true
+            
+            UserDefaults.standard.set(true, forKey: "shuffleState")
+        }
+    }
+    
+    @IBAction func repeatButtonTapped(_ sender: UIButton) {
+        if sender.isSelected == true{
+            sender.isSelected = false
+            repeatState = false
+            
+            UserDefaults.standard.set(false, forKey: "repeatState")
+        }
+        else{
+            sender.isSelected = true
+            repeatState = true
+            
+            UserDefaults.standard.set(true, forKey: "repeatState")
+        }
+    }
+
     func showMediaInfo(){
         let artistName = readArtistNameFromPlist(currentAudioIndex)
         let songName = readSongNameFromPlist(currentAudioIndex)
